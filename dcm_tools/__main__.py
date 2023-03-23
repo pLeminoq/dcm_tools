2dd8d33f4c9e1f9ecfbaeb795fb97f7f5932d2bc
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2023-03-23 15:48:22 +0100

    reformat so that it can be used as subcommands

---
+++ dcm_tools/__main__.py
@@ -0,0 +1,18 @@
+import argparse
+
+import dcm_tools.dump as dump
+import dcm_tools.send as send
+
+cmds = [dump, send]
+
+parser = argparse.ArgumentParser(description="DICOM tools", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparsers = parser.add_subparsers(required=True)
+
+for cmd in cmds:
+    _parser = subparsers.add_parser(cmd.cmd_name, description=cmd.cmd_desc)
+    cmd.add_args(_parser)
+    _parser.set_defaults(func=cmd.main)
+
+args = parser.parse_args()
+args.func(args)
+
--- dcm_tools/send.py
+++ dcm_tools/send.py
@@ -4,34 +4,12 @@ pre-configured DICOM nodes from a yaml file.
 """
 import argparse
 import subprocess
-from typing import Dict, List
+from typing import Any, Dict, List
 
 import yaml
 
-parser = argparse.ArgumentParser(
-    description="Send images to a DICOM node",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument(
-    "--node_yml",
-    type=str,
-    default="data/dcm_nodes.yml",
-    help="file containing definitions of available DICOM nodes",
-)
-parser.add_argument(
-    "--node",
-    type=str,
-    required=True,
-    help="an id, an alias or an AET matching to a node in the yaml file",
-)
-parser.add_argument(
-    "--sending_aet", type=str, default="GORILLA", help="the AET of the sending node"
-)
-parser.add_argument("--files", type=str, nargs="+", help="the files to send")
-args = parser.parse_args()
-
-with open(args.node_yml, mode="r") as f:
-    nodes = yaml.safe_load(f.read())
+cmd_name = "send"
+cmd_desc = "Send images to a DICOM node"
 
 
 def match_node(node: Dict[str, Any], _str: str) -> bool:
@@ -73,30 +51,71 @@ def match_node(node: Dict[str, Any], _str: str) -> bool:
     return False
 
 
-target_node = filter(lambda node: match_node(node, args.node), nodes)
-target_node = list(target_node)
+def add_args(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        "--node_yml",
+        type=str,
+        default="data/dcm_nodes.yml",
+        help="file containing definitions of available DICOM nodes",
+    )
+    parser.add_argument(
+        "--node",
+        type=str,
+        required=True,
+        help="an id, an alias or an AET matching to a node in the yaml file",
+    )
+    parser.add_argument(
+        "--sending_aet", type=str, default="GORILLA", help="the AET of the sending node"
+    )
+    parser.add_argument("files", type=str, nargs="+", help="the files to send")
 
-if len(target_node) == 0:
-    print(f"Could not find node {args.node} in definitions in {args.node_yml}")
-    exit(1)
-target_node = target_node[0]
 
-print(f"Send to node: {target_node}")
+def main(args):
+    with open(args.node_yml, mode="r") as f:
+        nodes = yaml.safe_load(f.read())
 
-target_aet = target_node["AET"]
-target_ip = target_node["IP"]
-target_port = target_node["Port"]
+    target_node = filter(lambda node: match_node(node, args.node), nodes)
+    target_node = list(target_node)
 
-for _file in args.files:
-    _args = [
-        "storescu",
-        "-v",
-        f"-aet {args.sending_aet}",
-        f"-aec {target_aet}",
-        str(target_ip),
-        str(target_port),
-        _file,
-    ]
-    _args_str = " ".join(_args)
-    print(f" - Run {_args_str}")
-    process = subprocess.run(_args_str, capture_output=True, check=True, shell=True)
+    if len(target_node) == 0:
+        print(f"Could not find node {args.node} in definitions in {args.node_yml}")
+        exit(1)
+    target_node = target_node[0]
+    print(f"Send to node: {target_node}")
+
+    target_aet = target_node["AET"]
+    target_ip = target_node["IP"]
+    target_port = target_node["Port"]
+
+    for _file in args.files:
+        _args = [
+            "storescu",
+            "-v",
+            f"-aet {args.sending_aet}",
+            f"-aec {target_aet}",
+            str(target_ip),
+            str(target_port),
+            _file,
+        ]
+        _args_str = " ".join(_args)
+        print(f" - Run {_args_str}")
+        process = subprocess.run(_args_str, capture_output=True, check=True, shell=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Send images to a DICOM node",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.set_defaults(func=main)
+
+    add_args(parser)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+
+
+
+
+
