fdb4c4ed3064152e311be798242409a8af88a6d4
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2023-03-23 15:47:58 +0100

    add dump comman

---
+++ dcm_tools/dump.py
@@ -0,0 +1,26 @@
+import argparse
+
+import pydicom
+
+cmd_name = "dump"
+cmd_desc = "Dump the header of a DICOM image"
+
+def add_args(parser: argparse.ArgumentParser):
+    parser.add_argument("file", type=str, help="the DICOM file whose header is dumped")
+
+def main(args):
+    print(args.file)
+    print(pydicom.dcmread(args.file))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=cmd_desc,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.set_defaults(func=main)
+
+    add_args(parser)
+
+    args = parser.parse_args()
+    args.func(args)
+
